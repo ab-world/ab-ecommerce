6c18c3af8b817e2189688fd6e8880efcc0747d55
HYUNSOOSHIN <shs0655@gmail.com>
2025-10-28 17:43:18 +0900

    feat: 프로젝트 개발환경 작업

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// 현재 파일 경로
+const __filename = fileURLToPath(import.meta.url);
+
+// 현재 디렉토리 경로
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src/style')]
+    }
 };
 
 export default nextConfig;
